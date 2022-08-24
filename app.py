f756b02cdcca35c3bbba27c51cffe7d799de3872
dineshkrishna9999 <67618093+dineshkrishna9999@users.noreply.github.com>
2022-08-24 15:52:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from jinja2 import escape
+app = Flask(__name__)
+
+#for demo purposes only to see different requests handled by instances of the application
+count = 0
+
+@app.route('/')
+def index():
+    global count
+    count += 1
+    return 'Hello from application ' + str(count)
+    #return 'Goodbye from application ' + str(count)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
